14a8fca153aa4ffe45718c4a93f1b93be10131b4
Matt Ernst <mernst.tviw@gmail.com>
2025-07-03 16:49:25 -0700

    Comment about generality

--- src/main/scala/wiki/extractor/types/LinkFeatures.scala
+++ src/main/scala/wiki/extractor/types/LinkFeatures.scala
@@ -1,5 +1,10 @@
 package wiki.extractor.types
 
+// This is missing "generality" from the original Milne implementation,
+// and the model suffers as a result. Generality was defined as link
+// distance from the "Fundamental categories" page. However, this page is
+// gone in recent Wikipedia dumps, so some other way of defining
+// generality may be needed.
 case class LinkModelEntry(
   sourcePageId: Int,
   label: String,
