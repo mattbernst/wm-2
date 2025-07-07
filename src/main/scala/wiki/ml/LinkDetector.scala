4586edf477b6456f577188ade9c1b623f16224d4
Matt Ernst <mernst.tviw@gmail.com>
2025-07-07 13:41:21 -0700

    Add LinkDetector

---
+++ src/main/scala/wiki/ml/LinkDetector.scala
@@ -0,0 +1,90 @@
+package wiki.ml
+
+import ai.catboost.{CatBoostModel, CatBoostPredictions}
+import wiki.extractor.language.types.NGram
+
+case class LabelLinkFeatures(
+  label: NGram,
+  linkedPageId: Int,
+  // Fields above are for bookkeeping. Fields below are model features.
+  normalizedOccurrences: Double,
+  maxDisambigConfidence: Double,
+  avgDisambigConfidence: Double,
+  relatednessToContext: Double,
+  relatednessToOtherTopics: Double,
+  linkProbability: Double,
+  firstOccurrence: Double,
+  lastOccurrence: Double,
+  spread: Double)
+
+case class LabelLinkPrediction(label: NGram, linkedPageId: Int, prediction: Double)
+
+class LinkDetector(catBoostModel: Array[Byte]) {
+
+  /**
+    * Apply a previously trained CatBoost model to predict whether a label
+    * extracted from a document would be a linked word or phrase in Wikipedia.
+    * This is dependent on features of the label and the larger context of
+    * the document it appears in. For example, the Wikipedia article at
+    * https://en.wikipedia.org/wiki/Mercury_sulfide
+    * contains the sentence
+    * "Mercury is produced from the cinnabar ore by roasting in air."
+    * The original Wikitext markup for this sentence is
+    * "Mercury is produced from the [[cinnabar]] ore by roasting in air."
+    *
+    * Only the term "cinnabar" is linked in this sentence in the original
+    * document. The trained model tries to predict which labels from plain text
+    * documents should be linked to a Wikipedia page.
+    *
+    * Before a label enters this link-detection stage, its word sense has
+    * already been chosen by the WordSenseDisambiguator. In the above sentence,
+    * "Mercury" would resolve to mercury the element rather than Mercury the
+    * planet or Mercury the god. If a label's sense has been mispredicted
+    * by the disambiguator, this can also impair predictions of its
+    * link-worthiness.
+    *
+    * A label's link-worthiness can be considered as a signal that it is an
+    * important topic related to the document; in Milne's original Wikipedia
+    * Miner, this logic was implemented in the TopicDetector.
+    *
+    * @param candidates An array of candidates with features, used for link
+    *                   prediction
+    * @return           An array of per-candidate predictions. Higher
+    *                   prediction score means more likely to be linked.
+    */
+  def predict(candidates: Array[LabelLinkFeatures]): Array[LabelLinkPrediction] = {
+    // Prepare numerical features.
+    // The feature order must exactly match the order used during training.
+    // See pysrc/train_linking.py and ExtractLinkTrainingData.scala
+    val numericalFeatures: Array[Array[Float]] = candidates.map { candidate =>
+      Array(
+        candidate.normalizedOccurrences.toFloat,
+        candidate.maxDisambigConfidence.toFloat,
+        candidate.avgDisambigConfidence.toFloat,
+        candidate.relatednessToContext.toFloat,
+        candidate.relatednessToOtherTopics.toFloat,
+        candidate.linkProbability.toFloat,
+        candidate.firstOccurrence.toFloat,
+        candidate.lastOccurrence.toFloat,
+        candidate.spread.toFloat
+      )
+    }
+
+    // Create a placeholder for categorical features to satisfy the method signature.
+    // For each row of numerical features, we provide an empty array of string features.
+    val categoricalFeatures: Array[Array[String]] = Array.fill(candidates.length)(Array())
+    val predictions: CatBoostPredictions          = model.predict(numericalFeatures, categoricalFeatures)
+    candidates.indices.toArray.map { j =>
+      val candidate = candidates(j)
+      val pred      = predictions.get(j, 0)
+      LabelLinkPrediction(
+        label = candidate.label,
+        linkedPageId = candidate.linkedPageId,
+        prediction = pred
+      )
+    }
+
+  }
+
+  private val model = CatBoostModel.loadModel(catBoostModel)
+}
